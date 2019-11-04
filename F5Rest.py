c845eac11de8ed020bd7f733dfdea2390a89cb97
Mitch Mahan <c-mitch.mahan@charter.com>
2019-11-04 11:51:07 -0700

    Added get pool stats command

--- F5Rest.py
+++ F5Rest.py
@@ -3,6 +3,7 @@ from robot.api import logger
 from robot.utils import ConnectionCache
 
 from f5.bigip import ManagementRoot
+from f5.utils.responses.handlers import Stats
 
 #logger.warn("Importing F5 REST library")
 
@@ -39,3 +40,49 @@ class F5Rest():
         command_list = str(commands).strip('[]')  # Allows lists as well as strings?
         cmd = '-c "zebos -r {} cmd {}"'.format(route_domain, command_list)
         return self.mgmt.tm.util.bash.exec_cmd('run', utilCmdArgs=cmd).commandResult
+
+    @keyword('get pool ${pool} stats')
+    def get_pool_stats(self,pool):
+        ''' HTTP GET a pool and its members. Load the member statistics
+            and return them in a dictionary.
+
+            {'/Common/example-server': {
+                'addr': {'description': '2001:200:0:1300::100'},
+                'connq_ageEdm': {'value': 0},
+                'connq_ageEma': {'value': 0},
+                'connq_ageHead': {'value': 0},
+                'connq_ageMax': {'value': 0},
+                'connq_depth': {'value': 0},
+                'connq_serviced': {'value': 0},
+                'curSessions': {'value': 0},
+                'monitorRule': {'description': 'none'},
+                'monitorStatus': {'description': 'unchecked'},
+                'nodeName': {'description': '/Common/example-server},
+                'poolName': {'description': '/Common/example-pool'},
+                'port': {'value': 80},
+                'serverside_bitsIn': {'value': 3998642720},
+                'serverside_bitsOut': {'value': 869489671944},
+                'serverside_curConns': {'value': 0},
+                'serverside_maxConns': {'value': 121},
+                'serverside_pktsIn': {'value': 6641290},
+                'serverside_pktsOut': {'value': 72555193},
+                'serverside_totConns': {'value': 98443},
+                'sessionStatus': {'description': 'enabled'},
+                'status_availabilityState': {'description': 'unknown'},
+                'status_enabledState': {'description': 'enabled'},
+                'status_statusReason': {'description': 'Pool member does not have service checking enabled'},
+                'totRequests': {'value': 98443}
+            }, ...
+        '''
+        stats = {}
+        my_pool = self.mgmt.tm.ltm.pools.pool.load(
+            partition='Common',
+            name=pool
+            )
+        my_pool_mbrs = my_pool.members_s.get_collection()
+        for pool_mbr in my_pool_mbrs:
+            mbr_stats = Stats(pool_mbr.stats.load())
+            dict_ = {mbr_stats.stat.nodeName.description: mbr_stats.stat}
+            stats.update(dict_)
+        return stats
+
--- Load Balancing/02__ssl_offload.robot
+++ Load Balancing/02__ssl_offload.robot
@@ -6,6 +6,20 @@ Variables   settings.yaml
 *** Test Cases ***
 V4 SSL Offload
     Reset Statistics
+    Log v4 Statistics
+    ${result}=     tmsh show ltm profile client-ssl clientssl | grep -i Protocol
+    # TLS 1.2 should be at 0 connections
+    Should Match Regexp     ${result}   Version 1.2.+0\n
+    # Start IXIA Test
+    # Wait until test completes
+    ${result}=     tmsh show ltm profile client-ssl clientssl | grep -i Protocol
+    # Should be thousands of connections "K"
+    Should Match Regexp     ${result}   Version 1.2.+K\n
+    &{stats}=   Get pool ${pool} stats
+    Log v4 Statistics
+
+V6 SSL Offload
+    Reset Statistics
     Log Statistics
     ${result} =     tmsh show ltm profile client-ssl clientssl | grep -i Protocol
     # TLS 1.2 should be at 0 connections
@@ -13,17 +27,27 @@ V4 SSL Offload
     # Start IXIA Test
     # Wait until test completes
     # Verify connections > 25K?
+    ${result} =     tmsh show ltm profile client-ssl clientssl | grep -i Protocol
+    # TLS 1.2 should have thousands of connections "K"
+    Should Match Regexp     ${result}   Version 1.2.+K\n
 
 *** Keywords ***
 Reset Statistics
-    [Documentation]     Reset statistics on the F5.
+    [Documentation]     Reset various statistics on the F5.
     tmsh reset-stats ltm virtual
     tmsh reset-stats ltm pool
     tmsh reset-stats ltm profile client-ssl clientssl
 
-Log Statistics
+Log v4 Statistics
     [Documentation]     Log statistics.
-    ${v1}=  tmsh show ltm pool ${pool}
+    ${v1}=  tmsh show ltm pool ${v4_pool}
     ${v2}=  tmsh show ltm virtual ${virtual_server}
+    ${v3}=  tmsh show ltm profile client-ssl clientssl | grep -i Protocol
+    Log Many    ${v1}   ${v2}   ${v3}
+
+Log v6 Statistics
+    [Documentation]     Log statistics.
+    ${v1}=  tmsh show ltm pool ${v6_pool}
+    ${v2}=  tmsh show ltm virtual ${v4_virtual_server}
     ${v3}=  tmsh show ltm profile client-ssl clientssl | grep -i Protocol
     Log Many    ${v1}   ${v2}   ${v3}
